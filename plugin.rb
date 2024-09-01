0e60c621bb9126f2f73b014f13d27914835691a6
khaled <khaledbadawi881@gmail.com>
2024-09-01 13:07:42 +0300

    update plugin name

--- plugin.rb
+++ plugin.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
-# about: TODO
+# name: discourse-test-plugin
+# about: test-plugin will display an alert message says "Welcome To Yanfaa Community"
 # meta_topic_id: TODO
 # version: 0.0.1
 # authors: Discourse
-# url: TODO
+# url: https://github.com/KhaledBadawyM/discourse-test-plugin.git
 # required_version: 2.7.0
 
 enabled_site_setting :plugin_name_enabled
 
 module ::MyPluginModule
-  PLUGIN_NAME = "discourse-plugin-name"
+  PLUGIN_NAME = "discourse-test-plugin"
 end
 
 require_relative "lib/my_plugin_module/engine"
