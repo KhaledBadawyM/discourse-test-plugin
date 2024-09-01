8ba193e6645fe0ee4655d719737d3927de435924
khaled <khaledbadawi881@gmail.com>
2024-09-01 10:52:31 +0300

    update plugin name

--- plugin.rb
+++ plugin.rb
@@ -1,5 +1,21 @@
-# name: basic-plugin
-# about: A super simple plugin to demonstrate how plugins work
+# frozen_string_literal: true
+
+# name: discourse-plugin-name
+# about: TODO
+# meta_topic_id: TODO
 # version: 0.0.1
-# authors: Awesome Plugin Developer
-# url: https://github.com/yourusername/basic-plugin
+# authors: Discourse
+# url: TODO
+# required_version: 2.7.0
+
+enabled_site_setting :plugin_name_enabled
+
+module ::MyPluginModule
+  PLUGIN_NAME = "discourse-plugin-name"
+end
+
+require_relative "lib/my_plugin_module/engine"
+
+after_initialize do
+  # Code which should run after Rails has finished booting
+end
